3d33f98db1e80fd603d96e9149c5b7b95c8eb128
clasarageth <getheljayce.clasara@gmail.com>
2026-02-08 20:39:37 +0800

    Update settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for projectsite project.
 
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/GethelJayce/PSUSphere/projectsite/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

9400ad14ef7546547d25815f92fe1dc98f1e5d65
clasarageth <getheljayce.clasara@gmail.com>
2026-02-08 20:52:19 +0800

    Update settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -116,8 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/home/GethelJayce/PSUSphere/projectsite/static/'
+SSTATIC_URL = '/static/'
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

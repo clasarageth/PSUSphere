1122ff021c0270d4f1cfe36efae7ec56a5f6fbf1
clasarageth <getheljayce.clasara@gmail.com>
2026-02-18 00:58:30 +0800

    List view for Organization

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -3,7 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.list import ListView
 from studentorg.models import Organization
+
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = 'organization'
+    template_name = 'org_list.html'
+    paginate_by = 5

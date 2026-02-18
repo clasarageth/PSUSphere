e9618091f90c44f04f2cb0215d856592e897b253
clasarageth <getheljayce.clasara@gmail.com>
2026-02-18 11:56:57 +0800

    orgmember listview

--- projectsite/studentorg/forms.py
+++ projectsite/studentorg/forms.py
@@ -5,3 +5,9 @@ class OrganizationForm(ModelForm):
     class Meta:
         model = Organization
         fields = "__all__"
+
+from .models import OrgMember
+class OrgMemberForm(ModelForm):
+    class Meta:
+        model = OrgMember
+        fields = "__all__"

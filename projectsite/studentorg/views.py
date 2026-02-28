747c04f6d024a726628afffef033edd2a79ff7b9
clasarageth <getheljayce.clasara@gmail.com>
2026-03-01 01:03:29 +0800

    set query

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -12,17 +12,48 @@ from studentorg.forms import StudentForm
 from studentorg.models import College
 from studentorg.forms import CollegeForm
 from django.urls import reverse_lazy
+from django.db.models import Q
+from django.utils import timezone
 
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
 
+    def get_context_data(self, **kwargs):
+    
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+    
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+    
+        context["students_joined_this_year"] = count
+        return context
+
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+    ordering = ["college__college_name","name"]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+    
+        if query:
+            qs = qs.filter (
+                Q(name__icontains=query) |
+                Q(description__icontains=query) 
+            )
 
 class OrganizationCreateView(CreateView):
     model = Organization
@@ -109,3 +140,4 @@ class CollegeDeleteView(DeleteView):
     model = College
     template_name = 'college_del.html'
     success_url = reverse_lazy('college-list')
+
